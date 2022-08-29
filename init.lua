e5af1cc5a0764ebf6142689c09d60fcfeb224649
nellfs-job <pokycelit@gmail.com>
2022-08-29 15:08:32 -0300

    init

---
+++ init.lua
@@ -0,0 +1,21 @@
+return require('packer').startup(function(use)
+
+use { "catppuccin/nvim", as = "catppuccin" }
+
+vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+
+require("catppuccin").setup()
+
+vim.cmd [[colorscheme catppuccin]]
+
+use {
+  "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }
+end)
+
