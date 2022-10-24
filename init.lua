d0dbfa7ddd0ef5f468d54d36473370cec951b7e3
nellfs <pokycelit@gmail.com>
2022-10-24 12:41:14 -0300

    opt

--- init.lua
+++ init.lua
@@ -1,9 +1,13 @@
 require('packer').startup(function(use)
 
 use { "ellisonleao/gruvbox.nvim" }
+use 'neovim/nvim-lspconfig' 
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
 
 end)
 
-
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
+vim.opt.number = true
+vim.opt.list = true
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
