6bed78486915008c4a4c3a8b66dee579285a4432
nellfs <nellfs@localhost.localdomain>
2023-05-26 20:44:02 -0300

    quotes

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -19,6 +19,16 @@ return require('packer').startup(function(use)
   -- packer manage itself
   use('wbthomason/packer.nvim')
 
+  use({
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+})
+
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
     requires = { { 'nvim-lua/plenary.nvim' } }
