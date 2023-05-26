f5965815dc0cf6ccb963f6776ecf7de75cdf1609
nellfs <pokycelit@gmail.com>
2023-05-25 22:12:10 -0300

    autoclose

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -1,53 +1,56 @@
-  -- packer bootstrapping
+-- packer bootstrapping
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
     vim.cmd [[packadd packer.nvim]]
     return true
   end
   return false
 end
 
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function(use)
   -- theme
-	use ("ellisonleao/gruvbox.nvim")
+  use("ellisonleao/gruvbox.nvim")
 
   -- packer manage itself
-  use ('wbthomason/packer.nvim')
+  use('wbthomason/packer.nvim')
 
   use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-  requires = { {'nvim-lua/plenary.nvim'} }
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
 
-  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-  use {'tpope/vim-fugitive'}
-  
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use { 'tpope/vim-fugitive' }
+  use 'm4xshen/autoclose.nvim'
+
 
   use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v2.x',
-  requires = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {                                      -- Optional
-      'williamboman/mason.nvim',
-      run = function()
-        pcall(vim.cmd, 'MasonUpdate')
-      end,
-    },
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' }, -- Required
+      { -- Optional
+        'williamboman/mason.nvim',
+        run = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},     -- Required
-    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-    {'L3MON4D3/LuaSnip'},     -- Required
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' }, -- Required
+      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+      { 'L3MON4D3/LuaSnip' }, -- Required
+    }
   }
-}
-	if packer_bootstrap then 
-	require("packer").sync()		
-	end
+  if packer_bootstrap then
+    require("packer").sync()
+  end
 end)
